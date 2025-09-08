f014cee572d64270ccdb3ea8980ce03d0fe63135
Ronald Yuhas <ryuhas@dynalab-inc.com>
2025-09-08 10:13:32 -0400

    Default the refresh rate to 1.

--- Task Bar Plus/Views/Controls/Utilities.xaml.cs
+++ Task Bar Plus/Views/Controls/Utilities.xaml.cs
@@ -33,31 +33,18 @@ namespace TaskBarPlus.Views.Controls
 
         private void RefreshRateComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
-            if (RefreshRateComboBox.SelectedItem is ComboBoxItem selectedItem &&
-            int.TryParse(selectedItem.Tag.ToString(), out int seconds))
-            {
-                // Use 'seconds' as the new refresh rate
-                Settings.Default.RefreshRate = seconds;
+            //if (RefreshRateComboBox.SelectedItem is ComboBoxItem selectedItem &&
+            //int.TryParse(selectedItem.Tag.ToString(), out int seconds))
+            //{
+            //    // Use 'seconds' as the new refresh rate
+            //    Settings.Default.RefreshRate = seconds;
                 Settings.Default.Save();
-            }
-        }
-
-        private void UtilityPopup_Opened(object sender, EventArgs e)
-        {
-            int savedRate = Settings.Default.RefreshRate;
-
-            foreach (ComboBoxItem item in RefreshRateComboBox.Items)
-            {
-                if (int.TryParse(item.Tag?.ToString(), out int tagValue) && tagValue == savedRate)
-                {
-                    RefreshRateComboBox.SelectedItem = item;
-                    break;
-                }
-            }
+            //}
         }
 
         private void Exit_Click(object sender, RoutedEventArgs e)
         {
+            Settings.Default.Save();
             Application.Current.Shutdown();
         }
     }
