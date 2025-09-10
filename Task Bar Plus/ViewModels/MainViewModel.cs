369f1a1949aacb28be6550d15c8602012886c26c
Dynalab-Ron <ryuhas@dynalab-inc.com>
2025-09-10 17:21:50 +0000

    Minimize if is in focus

--- Task Bar Plus/ViewModels/MainViewModel.cs
+++ Task Bar Plus/ViewModels/MainViewModel.cs
@@ -219,20 +219,27 @@ namespace TaskBarPlus.ViewModels
         {
             if (parameter is ApplicationItem item && item.MainWindowHandle != IntPtr.Zero)
             {
-                WINDOWPLACEMENT placement = new WINDOWPLACEMENT();
-                placement.length = Marshal.SizeOf(placement);
-
-                if (GetWindowPlacement(item.MainWindowHandle, ref placement))
+                IntPtr foreground = GetForegroundWindow();
+                if (foreground == item.MainWindowHandle)
                 {
-                    if (placement.showCmd == SW_SHOWNORMAL || placement.showCmd == SW_SHOWMAXIMIZED)
+                    // If app is already focused, minimize it
+                    ShowWindow(item.MainWindowHandle, SW_MINIMIZE);
+                }
+                else
+                {
+                    // Always maximize/restore and bring to front
+                    WINDOWPLACEMENT placement = new WINDOWPLACEMENT();
+                    placement.length = Marshal.SizeOf(placement);
+                    if (GetWindowPlacement(item.MainWindowHandle, ref placement))
                     {
-                        // App is visible, minimize it
-                        ShowWindow(item.MainWindowHandle, SW_MINIMIZE);
-                    }
-                    else
-                    {
-                        // App is minimized, restore it
-                        ShowWindow(item.MainWindowHandle, SW_RESTORE);
+                        if (placement.showCmd == SW_SHOWMINIMIZED)
+                        {
+                            ShowWindow(item.MainWindowHandle, SW_RESTORE);
+                        }
+                        else
+                        {
+                            ShowWindow(item.MainWindowHandle, SW_SHOWMAXIMIZED);
+                        }
                         SetForegroundWindow(item.MainWindowHandle);
                     }
                 }
